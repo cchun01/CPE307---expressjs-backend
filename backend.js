08f6d1ea992d981c8f86eb9e4d8e4db516671525
Colin Chun <colinchun@Colins-MacBook-Pro-2.local>
2022-01-10 13:36:37 -0800

    get users added

--- backend.js
+++ backend.js
@@ -42,7 +42,6 @@ const findUserByName = (name) => {
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    console.log(name);
     if (name != undefined){
     
         let result = findUserByName(name);
@@ -53,6 +52,22 @@ app.get('/users', (req, res) => {
         res.send(users);
     }
 });
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
