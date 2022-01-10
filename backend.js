78f441f8a7bf1f9075b6168619a0fcb325479834
Colin Chun <colinchun@Colins-MacBook-Pro-2.local>
2022-01-10 13:33:09 -0800

    get added

--- backend.js
+++ backend.js
@@ -4,8 +4,54 @@ const port = 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello f World!');
+const users = { 
+    users_list :
+    [
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       }
+    ]
+ }
+
+
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    console.log(name);
+    if (name != undefined){
+    
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
 
 app.listen(port, () => {
